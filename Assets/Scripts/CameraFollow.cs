5061bd80c481fcf1e8e48861bb27b429761c2119
Jacob Salzberg <jacob1818@gmail.com>
2018-03-11 06:10:48 -0300

    Add camera and player controller script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    Transform target; //our player
+    [SerializeField] // its related to our camera movement
+    float smoothing = 5f;
+
+    Vector3 offset; //offset between the player and the camera at the beggining
+
+    private void Awake()
+    {
+        Assert.IsNotNull(target);
+    }
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos,smoothing*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+    private CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
+	}
+}
