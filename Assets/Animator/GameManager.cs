1dcd8b89ba291b120962f2c70ac22919d0ba969b
Jacob Salzberg <jacob1818@gmail.com>
2018-03-11 23:20:51 -0300

    Create GameManager

---
+++ Assets/Animator/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    [SerializeField] GameObject player;
+
+    //alternative: public bool GameOver {get; private set;} 
+    private bool gameOver = false;
+
+    public bool GameOver
+    {
+        get
+        {
+            return gameOver;
+        }
+    }
+
+    private void Awake()
+    {
+        //make sure there's only one instance of gamemanager.cs
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this) 
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
