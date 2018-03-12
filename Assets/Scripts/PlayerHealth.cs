7596b4143d9b242a73c0730c7e55ca35768774ef
Jacob Salzberg <jacob1818@gmail.com>
2018-03-12 00:46:58 -0300

    Gtweaks

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,13 +4,62 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour {
 
+    [SerializeField] int startingHealth = 100;
+    [SerializeField] float timeSinceLastHit = 2f;
+
+    //time between hits
+    private float timer = 0f;
+    //once we die, stop being able to move
+    private CharacterController characterController;
+    //play death animation
+    private Animator anim;
+    private int currentHealth;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        anim = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        currentHealth = startingHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timer += Time.deltaTime;	
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (timer>= timeSinceLastHit && !GameManager.instance.GameOver)
+        {
+            if (other.tag =="Weapon") {
+                TakeHit();
+                //again -> timer = time since last hit
+                timer = 0; 
+            }
+        }
+    }
+
+    private void TakeHit()
+    {
+        if(currentHealth>0)
+        {
+            GameManager.instance.PlayerHit(currentHealth);
+            anim.Play("Hurt");
+            currentHealth -= 10;
+        }
+
+        if (currentHealth <=0)
+        {
+            KillPlayer();
+        }
+
+    }
+
+    private void KillPlayer()
+    {
+        GameManager.instance.PlayerHit(currentHealth);
+        anim.SetTrigger("HeroDie");
+        characterController.enabled = false;
+    }
 }
