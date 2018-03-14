9aeca4fa919daacfa3c520978512b8c458817c64
Jacob Salzberg <jacob1818@gmail.com>
2018-03-14 06:33:56 -0300

    Add menu scripts

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    [SerializeField] GameObject hero;
+    [SerializeField] GameObject tanker;
+    [SerializeField] GameObject soldier;
+    [SerializeField] GameObject ranger;
+
+
+    private Animator heroAnim;
+    private Animator tankerAnim;
+    private Animator soldierAnim;
+    private Animator rangerAnim;
+
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Showcase());
+        heroAnim = hero.GetComponent<Animator>();
+        tankerAnim = tanker.GetComponent<Animator>();
+        soldierAnim = soldier.GetComponent<Animator>();
+        rangerAnim = ranger.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Showcase()
+    {
+        yield return new WaitForSeconds(1f);
+        heroAnim.Play("SpinAttack");
+        yield return new WaitForSeconds(1f);
+        tankerAnim.Play("Attack");
+        yield return new WaitForSeconds(1f);
+        soldierAnim.Play("Attack");
+        yield return new WaitForSeconds(1f);
+        rangerAnim.Play("Attack");
+        yield return new WaitForSeconds(1f);
+
+        StartCoroutine(Showcase());
+    }
+}
